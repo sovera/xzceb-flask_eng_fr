d21515740a10694db29552d64653f67414fb99f1
Sovera <tiagoromanvera@gmail.com>
2023-06-20 18:59:09 -0300

    up

--- final_project/machinetranslation/tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from translator import frenchToEnglish, englishToFrench
-class TestSquare(unittest.TestCase): 
-    def test1(self): 
-        self.assertEqual(frenchToEnglish("Bonjour"), "Hello") 
-        
-class englishToFrench(unittest.TestCase): 
-    def test1(self): 
-        self.assertEqual(englishToFrench("Hello"), "Bonjour")
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,14 @@
+"""Modulo de traduccion de idiomas entre frances e ingles"""
 from deep_translator import MyMemoryTranslator
 
 def english_to_french(english_text):
-    traductor = MyMemoryTranslator(source='es', target='en')
+    """Funcion que traduce un texto de ingles a frances"""
+    traductor = MyMemoryTranslator(source='en', target='fr')
     french_text = traductor.translate(english_text)
     return french_text
 
 def french_to_english(french_text):
-    traductor = MyMemoryTranslator(source='es', target='en')
+    """Funcion que traduce un texto de frances a ingles"""
+    traductor = MyMemoryTranslator(source='fr', target='en')
     english_text = traductor.translate(french_text)
     return english_text
